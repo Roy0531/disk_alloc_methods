7e046d30c1d013ea127954744e902379a68db7da
Roy0531 <ryosuke.orito.53@gmail.com>
2023-11-01 14:20:46 -0500

    handler created

--- filesystem.h
+++ filesystem.h
@@ -1,4 +1,21 @@
+#include <iostream>
+using namespace std;
+
 class FileSystem {
 private:
+    int choice;
 public:
+    void handler(int choice){
+        switch(choice){
+            case 1: cout << "You chose 1" << endl; break;
+            case 2: cout << "You chose 2" << endl; break;
+            case 3: cout << "You chose 3" << endl; break;
+            case 4: cout << "You chose 4" << endl; break;
+            case 5: cout << "You chose 5" << endl; break;
+            case 6: cout << "You chose 6" << endl; break;
+            case 7: cout << "You chose 7" << endl; break;
+            case 8: cout << "Bye!\n" << endl; exit(0);
+            default: cout << "Please choose one of the options listed above" << endl; break;
+        }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -17,23 +17,13 @@ int main(int argc, char* argv[]) {
     }
 
     UserInterface ui;
+    FileSystem fs;
     
     string method = argv[1];
     ui.validateMethod(method);
 
     while(1){
-        int userChoice = ui.getUserOption();
-        
-        switch(userChoice){
-            case 1: cout << "You chose 1" << endl; break;
-            case 2: cout << "You chose 2" << endl; break;
-            case 3: cout << "You chose 3" << endl; break;
-            case 4: cout << "You chose 4" << endl; break;
-            case 5: cout << "You chose 5" << endl; break;
-            case 6: cout << "You chose 6" << endl; break;
-            case 7: cout << "You chose 7" << endl; break;
-            case 8: cout << "Bye!\n" << endl; return 0;
-            default: cout << "Please choose one of the options listed above" << endl; break;
-        }
+        int choice = ui.getUserOption();
+        fs.handler(choice);
     }
 }
